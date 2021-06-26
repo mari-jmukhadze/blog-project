f27adf94828dbfc005ba86282d766ae5206161af
mari-jmukhadze <m_jmukhadze@cu.edu.ge>
2021-06-26 09:35:39 +0400

    very simple styling. unfinished. have questions

--- script/script.js
+++ script/script.js
@@ -3,15 +3,15 @@ fetch('http://localhost:3000/api/blogs').then(res => res.json()).then(data => {
     console.log(data)
     data['data'].forEach(blog => {
         const row = document.createElement('div');
-        row.className = 'row'
+        row.className = 'row stylediv'
         const divuser = document.createElement('div')
-        divuser.className = 'col-lg-12'
+        divuser.className = 'col-lg-12 mt-3'
         const divblog = document.createElement('div')
         divblog.className = 'col-lg-12'
         const puser = document.createElement('p')
-        puser.className = 'text-center'
+        puser.className = 'text-center justify-content-center mystyle'
         const pblog = document.createElement('p')
-        pblog.className = 'text-center'
+        pblog.className = 'text-center mystyle blogstyle'
         console.log(blog)
 
         puser.innerHTML = `<span>${blog.Username}</span>`
